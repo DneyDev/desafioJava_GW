68d43e1298cdd5f0165a2fc67c59de528a80cd12
DneyDev <contdneydev@gmail.com>
2026-09-21 08:19:32 -0300

    adição de loop do Menu

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,25 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Para testar é só alterar os valores de null para o desejado
-        //Ordem de info: estado, cidade, cep, rua, numero 
-       /*  Endereco end = new Endereco("Pernambuco", "Recife", "35000-001", "Rua Tamanduá", "35" );
-        //ordem de info: name, cpf, email, end(Endereço)
-        Cliente cliente = new Cliente("Sidney", "012.345.678-90", "sidney@email.com", end);
-
-        //Para produto, é só ir enumerando e decidir os valores
-        //ordem de info: codigo, nome do produto, preço
-        Produto p1 = new Produto("01", "bola", 10.00);
-        Produto p2 = new Produto("02", "tesoura", 5.00);
-
-        //ordem de info: id de rastreio, cliente
-        Entrega entrega = new Entrega("BR01", cliente);
-        entrega.addProd(p1);
-        entrega.addProd(p2);
-
-        entrega.exibirResumo();
-        entrega.updStatus("A caminho");
-        */
         Scanner leitor = new Scanner(System.in);
         Sistema sistema = new Sistema(leitor);
 
@@ -37,7 +18,8 @@ public class Main {
         System.out.println("====================");
 
         int opcao = leitor.nextInt();
-        switch (opcao) {
+        do{
+            switch (opcao) {
             case 1:
                 sistema.cadastroCliente();
                 break;
@@ -59,6 +41,8 @@ public class Main {
                 System.out.println("4- Sair.");
                 System.out.println("====================");
                 break;
-        }
+            }
+        } while (opcao != 4);
+
     }
 }

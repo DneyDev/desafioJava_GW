39d81b4dd10970609771b9e9ee142becc7f24188
DneyDev <contdneydev@gmail.com>
2026-09-24 11:33:24 -0300

    fix: correção de falhas na validação de Cliente e Produto

--- src/validation/ProdutoValid.java
+++ src/validation/ProdutoValid.java
@@ -4,21 +4,30 @@ import src.models.Produto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProdutoValid implements Valid<Produto>{
+public class ProdutoValid implements Valid<Produto> {
 
-    @Override 
-    public List<String>validar(Produto produto){
-        List<String>erros = new ArrayList<>();
-        if(produto.getProdName() == null || produto.getProdName().isBlank()){
-            erros.add("Erro: Nome necessário");
-        }
-        if(produto.getPrice() <= 0){
-            erros.add("Erro: Preço do produto é obrigatório");
-        }
-        if(produto.getDesc() == null || produto.getDesc().isBlank()){
-            erros.add("Erro: Descrição é obrigatória.");
-        }
+    public String validarNome(String nome) {
+        return (nome == null || nome.isBlank()) ? "Nome é obrigatório!" : null;
+    }
+
+    public String validarDescricao(String desc) {
+        return (desc == null || desc.isBlank()) ? "Descrição é obrigatória!" : null;
+    }
+
+    public String validarPreco(double preco) {
+        return preco <= 0 ? "O preço deve ser maior que zero!" : null;
+    }
+
+    @Override
+    public List<String> validar(Produto produto) {
+        List<String> erros = new ArrayList<>();
+        addSeErro(erros, validarNome(produto.getProdName()));
+        addSeErro(erros, validarDescricao(produto.getDesc()));
+        addSeErro(erros, validarPreco(produto.getPrice()));
         return erros;
     }
-    
+
+    private void addSeErro(List<String> erros, String erro) {
+        if (erro != null) erros.add("Erro: " + erro);
+    }
 }
